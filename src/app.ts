01fc76231ebd055f79072c87a4564715c41a439a
Murtaza Vasi <murtazavasi2000@gmail.com>
2021-12-18 13:09:00 +0530

    Added validation for the user input

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,37 @@
+interface Validatable {
+	value: string | number;
+	required?: boolean;
+	minlength?: number;
+	maxlength?: number;
+	min?: number;
+	max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+	let isValid = true;
+	if (validatableInput.required) {
+		isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+	}
+
+	if (validatableInput.minlength != null && typeof validatableInput.value === 'string') {
+		isValid = isValid && validatableInput.value.length >= validatableInput.minlength;
+	}
+
+	if (validatableInput.maxlength != null && typeof validatableInput.value === 'string') {
+		isValid = isValid && validatableInput.value.length <= validatableInput.maxlength;
+	}
+
+	if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+		isValid = isValid && validatableInput.value >= validatableInput.min;
+	}
+
+	if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+		isValid = isValid && validatableInput.value <= validatableInput.max;
+	}
+
+	return isValid;
+}
+
 // AutoBind decorator
 function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
 	const originalMethod = descriptor.value;
@@ -11,7 +45,6 @@ function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
 	};
 	return newDescriptor;
 }
-
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLElement;
@@ -37,10 +70,50 @@ class ProjectInput {
 		this.attach();
 	}
 
+	private gatherUserInput(): [string, string, number] | void {
+		const title = this.titleInputElement.value;
+		const description = this.descriptionInputElement.value;
+		const people = this.peopleInputElement.value;
+
+		const titleValidatable: Validatable = {
+			value: title,
+			required: true,
+		};
+
+		const descriptionValidatable: Validatable = {
+			value: description,
+			required: true,
+			minlength: 5,
+		};
+
+		const peopleValidatable: Validatable = {
+			value: +people,
+			required: true,
+			min: 1,
+			max: 5,
+		};
+
+		if (validate(titleValidatable) && validate(descriptionValidatable) && validate(peopleValidatable)) {
+			return [title, description, +people];
+		} else {
+			alert('Invalid input! Please try again!');
+			return;
+		}
+	}
+
+	private clearInputs() {
+		this.titleInputElement.value = '';
+		this.peopleInputElement.value = '';
+		this.descriptionInputElement.value = '';
+	}
+
 	@AutoBind
 	private submitHandler(event: Event) {
 		event.preventDefault();
-		console.log(this.titleInputElement.value);
+		const userInput = this.gatherUserInput();
+
+		console.log(userInput);
+		this.clearInputs();
 	}
 
 	private configure() {
